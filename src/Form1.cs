6551f6e18540d7648cbe0a2f4e09a7191c43498c
ekeleze <78444619+ekeleze@users.noreply.github.com>
2024-04-16 22:03:03 -0500

    Yes

---
+++ src/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace LB_Command_Prompt
+{
+    public partial class Form1 : Form
+    {
+        Graphics g;
+
+        public Form1()
+        {
+            InitializeComponent();
+            g = this.CreateGraphics();
+        }
+
+        public void DrawChar(char c, int x, int y)
+        {
+
+        }
+
+        private void richTextBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            
+        }
+    }
+}
